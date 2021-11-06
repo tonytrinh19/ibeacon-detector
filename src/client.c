dc8c51d2e32effda87d9011b3fe9793f6b1e29e8
toni <tony.q.trinh@gmail.com>
2021-11-06 13:59:21 -0700

    Using template

---
+++ src/client.c
@@ -0,0 +1,150 @@
+#include <dc_application/command_line.h>
+#include <dc_application/config.h>
+#include <dc_application/defaults.h>
+#include <dc_application/environment.h>
+#include <dc_application/options.h>
+#include <dc_posix/dc_stdlib.h>
+#include <dc_posix/dc_string.h>
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct application_settings
+{
+    struct dc_opt_settings opts;
+    struct dc_setting_string *message;
+};
+
+
+
+static struct dc_application_settings *create_settings(const struct dc_posix_env *env, struct dc_error *err);
+static int destroy_settings(const struct dc_posix_env *env,
+                            struct dc_error *err,
+                            struct dc_application_settings **psettings);
+static int run(const struct dc_posix_env *env, struct dc_error *err, struct dc_application_settings *settings);
+static void error_reporter(const struct dc_error *err);
+static void trace_reporter(const struct dc_posix_env *env,
+                           const char *file_name,
+                           const char *function_name,
+                           size_t line_number);
+
+
+int main(int argc, char *argv[])
+{
+    dc_posix_tracer tracer;
+    dc_error_reporter reporter;
+    struct dc_posix_env env;
+    struct dc_error err;
+    struct dc_application_info *info;
+    int ret_val;
+
+    reporter = error_reporter;
+    tracer = trace_reporter;
+    tracer = NULL;
+    dc_error_init(&err, reporter);
+    dc_posix_env_init(&env, tracer);
+    info = dc_application_info_create(&env, &err, "Settings Application");
+    ret_val = dc_application_run(&env, &err, info, create_settings, destroy_settings, run, dc_default_create_lifecycle, dc_default_destroy_lifecycle, NULL, argc, argv);
+    dc_application_info_destroy(&env, &info);
+    dc_error_reset(&err);
+
+    return ret_val;
+}
+
+static struct dc_application_settings *create_settings(const struct dc_posix_env *env, struct dc_error *err)
+{
+    struct application_settings *settings;
+
+    DC_TRACE(env);
+    settings = dc_malloc(env, err, sizeof(struct application_settings));
+
+    if(settings == NULL)
+    {
+        return NULL;
+    }
+
+    settings->opts.parent.config_path = dc_setting_path_create(env, err);
+    settings->message = dc_setting_string_create(env, err);
+
+    struct options opts[] = {
+            {(struct dc_setting *)settings->opts.parent.config_path,
+                    dc_options_set_path,
+                    "config",
+                    required_argument,
+                    'c',
+                    "CONFIG",
+                    dc_string_from_string,
+                    NULL,
+                    dc_string_from_config,
+                    NULL},
+            {(struct dc_setting *)settings->message,
+                    dc_options_set_string,
+                    "message",
+                    required_argument,
+                    'm',
+                    "MESSAGE",
+                    dc_string_from_string,
+                    "message",
+                    dc_string_from_config,
+                    "Hello, Default World!"},
+    };
+
+    // note the trick here - we use calloc and add 1 to ensure the last line is all 0/NULL
+    settings->opts.opts_count = (sizeof(opts) / sizeof(struct options)) + 1;
+    settings->opts.opts_size = sizeof(struct options);
+    settings->opts.opts = dc_calloc(env, err, settings->opts.opts_count, settings->opts.opts_size);
+    dc_memcpy(env, settings->opts.opts, opts, sizeof(opts));
+    settings->opts.flags = "m:";
+    settings->opts.env_prefix = "DC_EXAMPLE_";
+
+    return (struct dc_application_settings *)settings;
+}
+
+static int destroy_settings(const struct dc_posix_env *env,
+                            __attribute__((unused)) struct dc_error *err,
+                            struct dc_application_settings **psettings)
+{
+    struct application_settings *app_settings;
+
+    DC_TRACE(env);
+    app_settings = (struct application_settings *)*psettings;
+    dc_setting_string_destroy(env, &app_settings->message);
+    dc_free(env, app_settings->opts.opts, app_settings->opts.opts_count);
+    dc_free(env, *psettings, sizeof(struct application_settings));
+
+    if(env->null_free)
+    {
+        *psettings = NULL;
+    }
+
+    return 0;
+}
+
+static int run(const struct dc_posix_env *env, struct dc_error *err, struct dc_application_settings *settings)
+{
+    struct application_settings *app_settings;
+    const char *message;
+
+    DC_TRACE(env);
+
+    app_settings = (struct application_settings *)settings;
+    message = dc_setting_string_get(env, app_settings->message);
+    printf("client says \"%s\"\n", message);
+
+    return EXIT_SUCCESS;
+}
+
+static void error_reporter(const struct dc_error *err)
+{
+    fprintf(stderr, "ERROR: %s : %s : @ %zu : %d\n", err->file_name, err->function_name, err->line_number, 0);
+    fprintf(stderr, "ERROR: %s\n", err->message);
+}
+
+static void trace_reporter(__attribute__((unused)) const struct dc_posix_env *env,
+                           const char *file_name,
+                           const char *function_name,
+                           size_t line_number)
+{
+    fprintf(stdout, "TRACE: %s : %s : @ %zu\n", file_name, function_name, line_number);
+}
