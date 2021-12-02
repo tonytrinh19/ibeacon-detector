78e1fdd9ce2dbf658601729efbe004d63bdc3a29
HyungJoon <itineris24@naver.com>
2021-12-01 19:24:07 -0800

    renew

---
+++ cmake-build-debug/src/CMakeFiles/ncurse.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ncurse.dir/ncurse.c.o"
+  "ncurse"
+  "ncurse.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ncurse.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/src/CMakeFiles/server.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/server.dir/common.c.o"
+  "CMakeFiles/server.dir/database.c.o"
+  "CMakeFiles/server.dir/server.c.o"
+  "server"
+  "server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
